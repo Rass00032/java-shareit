c722aecab7b1385113d2959ac45368fbd82d548c
Ravil <Rass00032@yandex.ru>
2023-08-01 17:58:42 +0400

    Исправлена обработка ошибок

--- src/main/java/ru/practicum/shareit/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/ErrorHandler.java
@@ -1,17 +1,13 @@
 package ru.practicum.shareit;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import ru.practicum.shareit.exception.BadStateException;
 import ru.practicum.shareit.exception.NotAvailableException;
 import ru.practicum.shareit.exception.NotFoundException;
 
